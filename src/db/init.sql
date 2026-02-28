5ddf06bab83f3416f5b2953d40d4e83db8f00bec
user <relax-swirl-decoy@duck.com>
2026-02-28 17:52:21 +0100

    added postgrestg

--- src/db/init.sql
+++ src/db/init.sql
@@ -3,10 +3,29 @@
 -- ** PostgreSQL version: 18.0
 -- ** Project Site: pgmodeler.io
 -- ** Model Author: ---
+-- object: postgrest | type: ROLE --
+-- DROP ROLE IF EXISTS postgrest;
+CREATE ROLE postgrest WITH 
+	LOGIN
+	 PASSWORD 'postgrest123';
+-- ddl-end --
+
 -- object: researcher | type: ROLE --
 -- DROP ROLE IF EXISTS researcher;
 CREATE ROLE researcher WITH 
 	LOGIN;
+-- ddl-end --
+
+-- object: webuser | type: ROLE --
+-- DROP ROLE IF EXISTS webuser;
+CREATE ROLE webuser WITH 
+	ROLE postgrest;
+-- ddl-end --
+
+-- object: anon | type: ROLE --
+-- DROP ROLE IF EXISTS anon;
+CREATE ROLE anon WITH 
+	ROLE postgrest;
 -- ddl-end --
 
 
@@ -15,7 +34,7 @@ CREATE ROLE researcher WITH
 
 -- object: postgres | type: DATABASE --
 -- DROP DATABASE IF EXISTS postgres;
-CREATE DATABASE postgres;
+-- CREATE DATABASE postgres;
 -- ddl-end --
 
 
@@ -26,6 +45,13 @@ CREATE SCHEMA auth;
 ALTER SCHEMA auth OWNER TO postgres;
 -- ddl-end --
 
+-- object: api | type: SCHEMA --
+-- DROP SCHEMA IF EXISTS api CASCADE;
+CREATE SCHEMA api;
+-- ddl-end --
+ALTER SCHEMA api OWNER TO postgres;
+-- ddl-end --
+
 -- object: data | type: SCHEMA --
 -- DROP SCHEMA IF EXISTS data CASCADE;
 CREATE SCHEMA data;
@@ -33,7 +59,7 @@ CREATE SCHEMA data;
 ALTER SCHEMA data OWNER TO postgres;
 -- ddl-end --
 
-SET search_path TO pg_catalog,public,auth,data;
+SET search_path TO pg_catalog,public,auth,api,data;
 -- ddl-end --
 
 -- object: postgis | type: EXTENSION --
@@ -46,14 +72,14 @@ WITH SCHEMA public;
 -- DROP TABLE IF EXISTS auth.users CASCADE;
 CREATE TABLE auth.users (
 	id uuid NOT NULL DEFAULT uuidv7(),
-	username text,
 	password text NOT NULL,
 	sys_created_at timestamptz NOT NULL DEFAULT NOW(),
-	sys_updated_at timestamptz NOT NULL DEFAULT NOW(),
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 ALTER TABLE auth.users OWNER TO postgres;
+-- ddl-end --
+ALTER TABLE auth.users ENABLE ROW LEVEL SECURITY;
 -- ddl-end --
 
 -- object: data.sensors | type: TABLE --
@@ -102,11 +128,36 @@ CREATE TABLE data.ownerships (
 ALTER TABLE data.ownerships OWNER TO postgres;
 -- ddl-end --
 
+-- object: data.features_of_interest | type: TABLE --
+-- DROP TABLE IF EXISTS data.features_of_interest CASCADE;
+CREATE TABLE data.features_of_interest (
+	id bigserial NOT NULL,
+	name text,
+	description text,
+	encoding_type text,
+	feature geography,
+	properties jsonb,
+	CONSTRAINT feature_of_interest_pk PRIMARY KEY (id)
+);
+-- ddl-end --
+ALTER TABLE data.features_of_interest OWNER TO postgres;
+-- ddl-end --
+
+-- object: data.participants | type: TABLE --
+-- DROP TABLE IF EXISTS data.participants CASCADE;
+CREATE TABLE data.participants (
+	user_id uuid NOT NULL,
+	CONSTRAINT participant_pk PRIMARY KEY (user_id)
+);
+-- ddl-end --
+ALTER TABLE data.participants OWNER TO postgres;
+-- ddl-end --
+
 -- object: data.observations | type: TABLE --
 -- DROP TABLE IF EXISTS data.observations CASCADE;
 CREATE TABLE data.observations (
 	id bigserial NOT NULL,
-	datastream_id bigint NOT NULL,
+	data_stream_id bigint NOT NULL,
 	phenomenon_time tstzrange,
 	result_time timestamptz,
 	valid_time tstzrange,
@@ -125,19 +176,41 @@ COMMENT ON COLUMN data.observations.valid_time IS E'The time period during which
 ALTER TABLE data.observations OWNER TO postgres;
 -- ddl-end --
 
--- object: data.features_of_interest | type: TABLE --
--- DROP TABLE IF EXISTS data.features_of_interest CASCADE;
-CREATE TABLE data.features_of_interest (
-	id bigserial NOT NULL,
-	name text,
-	description text,
-	encoding_type text,
-	feature geography,
-	properties jsonb,
-	CONSTRAINT feature_of_interest_pk PRIMARY KEY (id)
-);
+-- object: api.users | type: VIEW --
+-- DROP VIEW IF EXISTS api.users CASCADE;
+CREATE OR REPLACE VIEW api.users
+WITH (check_option=cascaded, security_invoker=true)
+AS 
+select * from data.participants;
 -- ddl-end --
-ALTER TABLE data.features_of_interest OWNER TO postgres;
+ALTER VIEW api.users OWNER TO postgres;
+-- ddl-end --
+
+-- object: allow_users_to_update_their_details | type: POLICY --
+-- DROP POLICY IF EXISTS allow_users_to_update_their_details ON auth.users CASCADE;
+CREATE POLICY allow_users_to_update_their_details ON auth.users
+	AS PERMISSIVE
+	FOR ALL
+	TO webuser
+	USING ((current_setting('request.jwt.claims', true)::json->>'id')::uuid = "id")
+	WITH CHECK ((current_setting('request.jwt.claims', true)::json->>'id')::uuid = "id");
+-- ddl-end --
+
+-- object: api.participant_observations | type: VIEW --
+-- DROP VIEW IF EXISTS api.participant_observations CASCADE;
+CREATE OR REPLACE VIEW api.participant_observations
+AS 
+select 
+	* 
+from 
+	data.participants p
+left join data.ownerships o on p.user_id = o.user_id
+left join data.sensors s on s.id = o.sensor_id
+left join data.data_stream d on d.sensor_id = s.id
+left join data.observations ob on ob.data_stream_id = d.id
+left join data.features_of_interest f on f.id = ob.features_of_interest_id;
+-- ddl-end --
+ALTER VIEW api.participant_observations OWNER TO postgres;
 -- ddl-end --
 
 -- object: data_stream_sensor_fk | type: CONSTRAINT --
@@ -150,7 +223,7 @@ ON DELETE RESTRICT ON UPDATE CASCADE;
 -- object: ownerships_users_fk | type: CONSTRAINT --
 -- ALTER TABLE data.ownerships DROP CONSTRAINT IF EXISTS ownerships_users_fk CASCADE;
 ALTER TABLE data.ownerships ADD CONSTRAINT ownerships_users_fk FOREIGN KEY (user_id)
-REFERENCES auth.users (id) MATCH SIMPLE
+REFERENCES data.participants (user_id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
@@ -163,7 +236,7 @@ ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- object: observations_datastreams_fk | type: CONSTRAINT --
 -- ALTER TABLE data.observations DROP CONSTRAINT IF EXISTS observations_datastreams_fk CASCADE;
-ALTER TABLE data.observations ADD CONSTRAINT observations_datastreams_fk FOREIGN KEY (datastream_id)
+ALTER TABLE data.observations ADD CONSTRAINT observations_datastreams_fk FOREIGN KEY (data_stream_id)
 REFERENCES data.data_stream (id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
@@ -175,9 +248,88 @@ REFERENCES data.features_of_interest (id) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 -- ddl-end --
 
--- object: grant_rawd_ce95c97fcb | type: PERMISSION --
+-- object: participants_users_fk | type: CONSTRAINT --
+-- ALTER TABLE data.participants DROP CONSTRAINT IF EXISTS participants_users_fk CASCADE;
+ALTER TABLE data.participants ADD CONSTRAINT participants_users_fk FOREIGN KEY (user_id)
+REFERENCES auth.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "grant_U_72fefecc16" | type: PERMISSION --
+GRANT USAGE
+   ON SCHEMA data
+   TO webuser,anon;
+
+-- ddl-end --
+
+
+-- object: grant_rawd_87ad18c88a | type: PERMISSION --
 GRANT SELECT,INSERT,UPDATE,DELETE
-   ON TABLE auth.users
+   ON TABLE data.ownerships
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_rawd_9c47979853 | type: PERMISSION --
+GRANT SELECT,INSERT,UPDATE,DELETE
+   ON TABLE data.sensors
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_r_35bae7d3cf | type: PERMISSION --
+GRANT SELECT
+   ON TABLE data.data_stream
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_r_d1632fdea1 | type: PERMISSION --
+GRANT SELECT
+   ON TABLE data.observations
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_r_a6107bdbdb | type: PERMISSION --
+GRANT SELECT
+   ON TABLE data.features_of_interest
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_r_4fa48971dc | type: PERMISSION --
+GRANT SELECT
+   ON TABLE api.participant_observations
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: "grant_U_dae521e4e3" | type: PERMISSION --
+GRANT USAGE
+   ON SCHEMA api
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_rw_afd8703543 | type: PERMISSION --
+GRANT SELECT,UPDATE
+   ON TABLE api.users
+   TO webuser;
+
+-- ddl-end --
+
+
+-- object: grant_r_fe0574898d | type: PERMISSION --
+GRANT SELECT
+   ON TABLE api.participant_observations
    TO researcher;
 
 -- ddl-end --
