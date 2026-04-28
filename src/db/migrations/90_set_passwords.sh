9f69e002647288bad2a2a7627efead507d36674c
user <relax-swirl-decoy@duck.com>
2026-04-28 02:30:49 +0200

    passwords

--- src/db/migrations/90_set_passwords.sh
+++ src/db/migrations/90_set_passwords.sh
@@ -5,4 +5,5 @@ echo "Setting passwords..."
 psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     ALTER ROLE postgrest WITH PASSWORD '${POSTGREST_DB_PASSWORD}';
     ALTER ROLE pipeline WITH PASSWORD '${PIPELINE_DB_PASSWORD}';
+    ALTER ROLE researcher WITH PASSWORD '${RESEARCHER_DB_PASSWORD}';
 EOSQL
